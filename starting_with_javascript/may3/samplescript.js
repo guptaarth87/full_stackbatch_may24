192177af1fc943c3bfdd4179184b084748294dc4
guptaarth87 <guptaarth87@gmail.com>
2024-05-03 21:57:15 +0530

    starting js

---
+++ starting_with_javascript/may3/samplescript.js
@@ -0,0 +1,85 @@
+console.log("arth")
+
+console.log("i am arth")
+console.log("hello world")
+
+/* String
+Number
+boolean -> True , Flase
+
+Unidentified -> a=10
+null     var a = null
+bigint */
+
+/* a= "arth"
+console.log(a)   // -> arth
+console.log("a")  // -> a
+
+let num = 10.100
+console.log(num)
+
+var booval = true
+console.log(booval) */
+
+
+/* var b = 10.100
+print( b) */
+
+/* 
+we use 3 datatypes to declare
+1. let
+2. const
+3. var
+
+
+we will learn difference on the basis of two parameters
+1. Mutability
+2. scope
+
+1-> any variable declared using const cannot be changed */
+
+
+/* 
+const a = 10;
+a=20;
+console.log(a) */
+
+/* 
+let a = 10;
+a=20;
+console.log(a) */
+
+/* how var and let are different */
+
+
+/* scope */
+
+/* var b=10
+if (b==10){
+    let a= 10
+    console.log(a)
+}
+console.log(a) */
+
+
+let have a scope till opening and closing curly brackets
+but var have a functional scope
+
+
+
+function printhello(){
+      /* var a= 11;
+       console.log(a) */
+      var b=10
+      if (b==10){
+          let a= 10;
+          console.log(a)
+       } 
+       /* console.log(a) */
+      
+}
+
+printhello()
+/* console.log(a) */
+
+
